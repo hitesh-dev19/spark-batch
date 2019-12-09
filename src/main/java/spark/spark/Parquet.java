3b7c542a73e3646851c6e4d78d84b200151a54aa
hitesh-dev19 <57974072+hitesh-dev19@users.noreply.github.com>
2019-12-08 19:54:04 -0500

    Update Parquet.java

--- src/main/java/spark/spark/Parquet.java
+++ src/main/java/spark/spark/Parquet.java
@@ -30,42 +30,8 @@ public class Parquet {
 		Dataset<Row> csv = session.read().option("header", "true").csv("s3n://amazonsparkemr//inputFiles//Summary of Weather.csv");
 		String start = new SimpleDateFormat("HHmmss").format(Calendar.getInstance().getTime());
 		System.out.println(csv.count() );
-		//csv.printSchema();
 		
-		//csv.write().save("C:\\\\\\\\Users\\\\\\\\Pratik Joshi\\\\\\\\Desktop\\\\\\\\spark\\\\\\\\weather1");
-		//csv.write().mode("overwrite").  parquet("C:\\\\Users\\\\Pratik Joshi\\\\Desktop\\\\spark\\\\weather");
-		//csv.write().format("parquet").mode("overwrite"). saveAsTable("weather");
-		/*String end = new SimpleDateFormat("HHmmss").format(Calendar.getInstance().getTime());
-		
-		Dataset<Row> sql = session.sql("select * from weather where YR='41'");
-		
-		System.out.println("count: "+sql.count());
-		
-		
-		System.out.println(start+" "+end);
-		System.out.println(Integer.parseInt(end)-Integer.parseInt(start) );
-		System.out.println("Execution Completed");
-		
-		
-		
-		Dataset<customerPoJo> csv2 = session.read(). csv("C:\\\\Users\\\\Pratik Joshi\\\\Desktop\\\\spark\\\\sampleFile.csv").map(row -> {
-			return new customerPoJo(row.getString(0), row.getString(1), row.getString(2), row.getString(3));}
-		, Encoders.bean(customerPoJo.class));
-		
-		csv2.write().option("compression", "uncompressed").mode("overwrite").save("c:\\Users\\Pratik Joshi\\Desktop\\spark\\datalzo");
-		
-		System.out.println("execution completed");
-		
-		Dataset<Row> parquet = session.read().parquet("c:\\Users\\Pratik Joshi\\Desktop\\spark\\datalzo");
-		
-	//	parquet.filter("id>2000").write().save("c:\\\\Users\\\\Pratik Joshi\\\\Desktop\\\\spark\\\\dataFilter");
-		
-		
-		Dataset<Row> parquet2 = session.read().parquet("c:\\\\\\\\Users\\\\\\\\Pratik Joshi\\\\\\\\Desktop\\\\\\\\spark\\\\\\\\dataFilter");
-		
-		System.out.println("count:"+parquet2.count());
-		
-		parquet.show(); */
+	
 
 	}
 
